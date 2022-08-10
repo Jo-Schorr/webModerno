4922d70895493a26fca586c9adfe376d4df25ea3
Schorr <josueschorr@hotmail.com>
2022-08-10 13:03:37 -0300

    dados_1

---
+++ dados_1.js
@@ -0,0 +1,18 @@
+// var armazena  uma informacao
+// também podemos utilizar  $ let como variavel 
+
+var nome = "Caneta";
+var quantidade = 10; 
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+
+console.log('Valor total da Caneta',precoFinal);
