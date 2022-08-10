01548a77c0cf23290434ded321693df1b3a50b0d
Schorr <josueschorr@hotmail.com>
2022-08-10 12:21:19 -0300

    init

---
+++ comentario.js
@@ -0,0 +1,17 @@
+// console.log é usado  para exibir algo na tela  do computador
+
+console.log("teste1");
+console.log("teste2"); // mais um exemplo de console.log
+console.log("teste3");
+
+// console.log('Passo #02');
+
+/*
+* Esse é um 
+* comentário
+* de multiplas 
+* linhas
+*/
+
+
+console.log("Passo  #02");
