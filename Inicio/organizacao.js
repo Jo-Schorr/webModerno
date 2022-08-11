3b24a6ed716d13d73e98610002f46f7781b0603f
Schorr <josueschorr@hotmail.com>
2022-08-11 17:38:24 -0300

    init

---
+++ Inicio/organizacao.js
@@ -0,0 +1,20 @@
+/*
+JS é organizado em sentença que podem ou nao
+terminar com um ponto e virgula e também é organizado
+em blocosa de código
+esses códidos sao delimitados  por Chaves.
+
+*/
+
+
+
+
+
+console.log("Setença de código")
+
+{
+   {
+console.log("OlÁ");
+console.log ("'Mundos') // Pdrao  do curso
+   }
+}
