4a50194ffb8e39a0bdcebbf959b964705b6ff3b3
Schorr <josueschorr@hotmail.com>
2022-08-11 22:16:06 -0300

    strings

---
+++ fundamentos-JS/strings.js
@@ -0,0 +1,17 @@
+const escola = "Cod3r"
+
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(3))
+console.log(escola.charAt(2))
+console.log(escola.charAt(1))
+console.log(escola.charAt(0))
+
+
+
+
+console.log(escola.charAt(0))
+console.log(escola.charAt(1))
+console.log(escola.charAt(2))
+console.log(escola.charAt(3))
+console.log(escola.charAt(4))
