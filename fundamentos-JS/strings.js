91b6626f91c274ef76a0456ed61bbf9ff7dac84d
Schorr <josueschorr@hotmail.com>
2022-08-11 22:39:26 -0300

    string

--- fundamentos-JS/strings.js
+++ fundamentos-JS/strings.js
@@ -1,17 +1,19 @@
-const escola = "Cod3r"
+const escola = "Cod3r";
 
+console.log(escola.charAt(4));
+console.log(escola.charAt(3));
+//console.log(escola.charAt(2))
+//console.log(escola.charAt(1))
+//console.log(escola.charAt(0))
 
-console.log(escola.charAt(4))
-console.log(escola.charAt(3))
-console.log(escola.charAt(2))
-console.log(escola.charAt(1))
-console.log(escola.charAt(0))
+console.log(escola.charCodeAt(3));
+console.log(escola.indexOf("3"));
 
+console.log(escola.substring(1));
+console.log(escola.substring(0, 3));
 
+console.log("Escola ".concat(escola).concat("!"));
 
+console.log(escola.replace(3, "e"));
 
-console.log(escola.charAt(0))
-console.log(escola.charAt(1))
-console.log(escola.charAt(2))
-console.log(escola.charAt(3))
-console.log(escola.charAt(4))
+console.log("Ana,Maria,Pedro".split(","));
