60f032abc99ed9ecbc5c52558b25c315321120ab
Schorr <josueschorr@hotmail.com>
2022-08-12 22:51:42 -0300

    init

--- fundamentos-JS/Object.js
+++ fundamentos-JS/Object.js
@@ -10,10 +10,19 @@ console.log(prod1)
 
 
 const prod2 = {
+     nome:'Camisa Polo',
+     preco: 79.90,
+     obj: {
+        blabla:1,
 
+        obj:{
+            blabla:2,
 
-
+        }
+     }
 
 }
 
 
+
+console.log(prod2)
