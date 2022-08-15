f2d22a85aef425e1fe32981aed4e9732fa8f891c
Schorr <josueschorr@hotmail.com>
2022-08-15 08:07:26 -0300

    init

---
+++ fundamentos-JS/objeto2.js
@@ -0,0 +1,13 @@
+
+// function seria  o presidente
+// objeto seria o vice presidente
+
+console.log(typeof Object)
+
+console.log(typeof new Object)
+
+const Cliente = function () {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+
---
+++ fundamentos-JS/usandoVarEmLoop1.js
@@ -0,0 +1,5 @@
+for (var i = 0; i <19; ++i){
+      console.log(i)
+
+    }
+console.log('i =', i)
